f606310062508061c96fd8c294084b9a38c94ae3
Gianluca Venturini <gianluca@stellarite.io>
2021-11-20 20:21:23 -0800

    First implementation of Gene

---
+++ src/gene.rs
@@ -0,0 +1,158 @@
+use std::fmt::{self, Debug, Formatter};
+
+pub struct Gene {
+	// source neuron
+	source: u8,
+	// destination neuron
+	destination: u8,
+	pub weight: i16,
+}
+
+pub struct Brain {
+	pub num_input: u8,
+	pub num_internal: u8,
+	pub num_output: u8,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum NeuronType {
+	Input,
+	Internal,
+	Output,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Neuron {
+	neuron_type: NeuronType,
+	neuron_number: u8,
+}
+
+impl Debug for Gene {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		write!(f, "{} {} {}", self.source, self.destination, self.weight)
+	}
+}
+
+impl Gene {
+	pub fn init(source: u8, destination: u8, weight: i16) -> Gene {
+		return Gene {
+			source,
+			destination,
+			weight,
+		};
+	}
+
+	pub fn get_source_neuron_type(&self) -> NeuronType {
+		if self.source & 0b10000000 == 0 {
+			return NeuronType::Input;
+		} else {
+			return NeuronType::Internal;
+		}
+	}
+
+	pub fn get_destination_neuron_type(&self) -> NeuronType {
+		println!("{}", self.destination & 0b10000000);
+		if self.destination & 0b10000000 == 0 {
+			return NeuronType::Internal;
+		} else {
+			return NeuronType::Output;
+		}
+	}
+
+	pub fn get_source_neuron(&self, brain: &Brain) -> Neuron {
+		let neuron_type = self.get_source_neuron_type();
+		Gene::get_neuron(neuron_type, self.source, brain)
+	}
+
+	pub fn get_destination_neuron(&self, brain: &Brain) -> Neuron {
+		let neuron_type = self.get_destination_neuron_type();
+		Gene::get_neuron(neuron_type, self.destination, brain)
+	}
+
+	fn get_neuron(neuron_type: NeuronType, raw_number: u8, brain: &Brain) -> Neuron {
+		let neuron_number = match neuron_type {
+			NeuronType::Input => (raw_number & 0b01111111) % brain.num_input,
+			NeuronType::Internal => (raw_number & 0b01111111) % brain.num_input,
+			NeuronType::Output => (raw_number & 0b01111111) % brain.num_output,
+		};
+		Neuron {
+			neuron_type,
+			neuron_number,
+		}
+	}
+}
+
+#[test]
+fn should_select_source_type() {
+	assert_eq!(
+		Gene::init(0, 0, 0).get_source_neuron_type(),
+		NeuronType::Input
+	);
+	assert_eq!(
+		Gene::init(128, 0, 0).get_source_neuron_type(),
+		NeuronType::Internal
+	);
+}
+
+#[test]
+fn should_select_destination_type() {
+	assert_eq!(
+		Gene::init(0, 0, 0).get_destination_neuron_type(),
+		NeuronType::Internal
+	);
+	assert_eq!(
+		Gene::init(0, 128, 0).get_destination_neuron_type(),
+		NeuronType::Output
+	);
+}
+
+#[test]
+fn should_select_source_neuron() {
+	let brain = Brain {
+		num_input: 5,
+		num_output: 5,
+		num_internal: 5,
+	};
+	assert_eq!(
+		Gene::init(0, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Input,
+			neuron_number: 0
+		}
+	);
+	assert_eq!(
+		Gene::init(1, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Input,
+			neuron_number: 1
+		}
+	);
+	assert_eq!(
+		Gene::init(5, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Input,
+			neuron_number: 0
+		}
+	);
+	assert_eq!(
+		Gene::init(128, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Internal,
+			neuron_number: 0
+		}
+	);
+	assert_eq!(
+		Gene::init(128 + 1, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Internal,
+			neuron_number: 1
+		}
+	);
+	assert_eq!(
+		Gene::init(128 + 5, 0, 0).get_source_neuron(&brain),
+		Neuron {
+			neuron_type: NeuronType::Internal,
+			neuron_number: 0
+		}
+	);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod gene;
+
 fn main() {
     println!("Hello, world!");
+    println!("{:?}", gene::Gene::init(4, 5, 0));
 }
